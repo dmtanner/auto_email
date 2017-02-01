c71d364c80d9745acd937cd5c7364db4f548133c
dmtanner <kritiosboy@gmail.com>
2017-02-01 09:33:05 -0700

    parameterized more inputs (untested)

--- emailer.py
+++ emailer.py
@@ -25,15 +25,20 @@ def send_email(user, pwd, recipient, subject, body):
 	print sys.exc_info()[0]
 
 
+my_name = raw_input("Your Name: ")
 my_email = raw_input("Your Email: ")
 my_password = raw_input("Email Password: ")
+my_bishop = raw_input("Your Bishop's Last Name: ")
+my_bishop_email = raw_input("Your Bishop's Email: ")
+my_bishop_phone = raw_input("Your Bishop's Phone #: ")
+my_ward = raw_input("Your Ward Name: ")
 
 while(1):
-	ward_member = raw_input('Ward Member: ')
-	bishop_last_name = raw_input('Bishop Last Name: ')
-	bishop_email = raw_input('Bishop email: ')
+	ward_member = raw_input('New Ward Member: ')
+	previous_bishop_last_name = raw_input('Former Bishop Last Name: ')
+	previous_bishop_email = raw_input('Former Bishop email: ')
 
-	email_msg = 'Bishop %s,\n\nThe Provo YSA 91st Ward in Provo, Utah has received the membership records of %s and would like to extend a calling as soon as possible.\n\nIf you have any comments about strengths, or concerns regarding a calling being extended, a temple recommend being issued, or an ecclesiastical endorsement given to this member, please email Bishop Hansen at:\n\n\t\tjeff@johnnys.com\n\nOr call or text at 801-471-4834\n\nThank you,\nMarcus Tanner\nAssistant Clerk\nProvo YSA 91st Ward' % (bishop_last_name, ward_member)
+	email_msg = 'Bishop %s,\n\nThe %s Ward has received the membership records of %s and would like to extend a calling as soon as possible.\n\nIf you have any comments about strengths, or concerns regarding a calling being extended, a temple recommend being issued, or an ecclesiastical endorsement given to this member, please email Bishop %s at:\n\n\t\t%s\n\nOr call or text at %s\n\nThank you,\n%s\nWard Clerk\n%s' % (previous_bishop_last_name, my_ward, ward_member, my_bishop, my_bishop_email, my_bishop_phone, my_name, my_ward)
 
-	send_email(my_email, my_password, bishop_email, ward_member + ' Prior Unit Ward Calling', email_msg)
+	send_email(my_email, my_password, previous_bishop_email, ward_member + ' Prior Unit Ward Calling', email_msg)
 	print
